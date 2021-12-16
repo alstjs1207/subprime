e137d92a03ea219d39927ae982276738a37b91e1
alstjs1207 <alstjs1207@naver.com>
2021-12-16 21:54:23 +0900

    first commit

---
+++ src/main/java/com/yulotte/subprime/SubprimeApplication.java
@@ -0,0 +1,13 @@
+package com.yulotte.subprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubprimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubprimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yulotte/subprime/SubprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulotte.subprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubprimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
