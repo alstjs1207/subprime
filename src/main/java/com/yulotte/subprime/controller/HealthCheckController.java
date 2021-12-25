de8f0bf44d505621856b2e58d0f5b6e30a1cc83b
alstjs1207 <alstjs1207@naver.com>
2021-12-25 15:39:36 +0900

    first commit

---
+++ src/main/java/com/yulotte/subprime/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.yulotte.subprime.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yulotte
+ */
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("api/ping")
+    public String ping() {
+        return "ok";
+    }
+}
