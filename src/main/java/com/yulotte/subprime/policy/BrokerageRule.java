e22d5c4137402040a81a9f5c630e450209cab9c6
alstjs1207 <alstjs1207@naver.com>
2022-01-02 17:24:30 +0900

    add purchase broerage

---
+++ src/main/java/com/yulotte/subprime/policy/BrokerageRule.java
@@ -0,0 +1,29 @@
+package com.yulotte.subprime.policy;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+/**
+ * 상한효율과 상한금액
+ */
+public class BrokerageRule {
+    private Double brokeragePercent;
+    private Long limitAmount;
+
+    public Long calcMaxBrokerage(Long price) {
+        if (limitAmount == null) {
+            return multiplyPercent(price);
+        }
+        return Math.min(multiplyPercent(price), limitAmount);
+    };
+
+    private Long multiplyPercent(Long price) {
+        return Double.valueOf(Math.floor(price * (brokeragePercent / 100))).longValue();
+    }
+
+    @Builder
+    BrokerageRule(Double brokeragePercent, Long limitAmount) {
+        this.brokeragePercent = brokeragePercent;
+        this.limitAmount = limitAmount;
+    }
+}
---
+++ src/main/java/com/yulotte/subprime/policy/RentBrokeragePolicy.java
@@ -0,0 +1,4 @@
+package com.yulotte.subprime.policy;
+
+public class RentBrokeragePolicy {
+}
