b4540b8afcf38e2f8be73f92e67dc7920000423a
alstjs1207 <alstjs1207@naver.com>
2022-01-02 17:50:31 +0900

    refactoring brokerage policy interface

---
+++ src/main/java/com/yulotte/subprime/policy/BrokeragePolicy.java
@@ -0,0 +1,10 @@
+package com.yulotte.subprime.policy;
+
+public interface BrokeragePolicy {
+    BrokerageRule createBrokerageRule(Long price);
+
+     default Long calculate(Long price) {
+        BrokerageRule brokerageRule = createBrokerageRule(price);
+        return brokerageRule.calcMaxBrokerage(price);
+    };
+}
--- src/main/java/com/yulotte/subprime/policy/RentBrokeragePolicy.java
+++ src/main/java/com/yulotte/subprime/policy/RentBrokeragePolicy.java
@@ -1,15 +1,8 @@
 package com.yulotte.subprime.policy;
 
-public class RentBrokeragePolicy {
-    public Long calculate(Long price) {
-        /**
-         * 생성과 계산을 분리합니다.
-         */
-        BrokerageRule brokerageRule = createBrokerageRule(price);
-        return brokerageRule.calcMaxBrokerage(price);
-    }
+public class RentBrokeragePolicy implements BrokeragePolicy{
 
-    private BrokerageRule createBrokerageRule(Long price) {
+    public BrokerageRule createBrokerageRule(Long price) {
         BrokerageRule rule;
         if (price < 50_000_000) {
             rule = BrokerageRule.builder().brokeragePercent(0.5).limitAmount(200_000L).build();
