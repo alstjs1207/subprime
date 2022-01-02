a6c8bf431a1e5d2b79856754ef323a226b629013
alstjs1207 <alstjs1207@naver.com>
2022-01-02 18:30:21 +0900

    refactoring factory pattern

--- src/main/java/com/yulotte/subprime/controller/BrokerageQueryController.java
+++ src/main/java/com/yulotte/subprime/controller/BrokerageQueryController.java
@@ -1,6 +1,8 @@
 package com.yulotte.subprime.controller;
 
 import com.yulotte.subprime.constants.ActionType;
+import com.yulotte.subprime.policy.BrokeragePolicy;
+import com.yulotte.subprime.policy.BrokeragePolicyFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BrokerageQueryController {
 
+
     @GetMapping("/api/calc/brokerage")
     public Long calcBrokerage(@RequestParam ActionType actionType, @RequestParam Long price) {
-        //타입 정의 매매? 임대?
-        // TODO: 중개 수수료 계산 로직
-        return null;
+        // TODO: action type 에 따라 계산하도록 수정
+        BrokeragePolicy policy = BrokeragePolicyFactory.getBrokeragePolicyBy(actionType);
+        return policy.calculate(price);
     }
 }
--- src/main/java/com/yulotte/subprime/policy/BrokeragePolicy.java
+++ src/main/java/com/yulotte/subprime/policy/BrokeragePolicy.java
@@ -3,8 +3,8 @@ package com.yulotte.subprime.policy;
 public interface BrokeragePolicy {
     BrokerageRule createBrokerageRule(Long price);
 
-     default Long calculate(Long price) {
+    default Long calculate(Long price) {
         BrokerageRule brokerageRule = createBrokerageRule(price);
         return brokerageRule.calcMaxBrokerage(price);
-    };
+    }
 }
