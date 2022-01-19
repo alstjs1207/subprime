cd76d8c6a3b583b7fe9be3780ebb6bc536ebe15a
alstjs1207 <alstjs1207@naver.com>
2022-01-19 20:52:10 +0900

    add brokerage apartment api

--- src/main/java/com/yulotte/subprime/controller/BrokerageQueryController.java
+++ src/main/java/com/yulotte/subprime/controller/BrokerageQueryController.java
@@ -4,6 +4,7 @@ import com.yulotte.subprime.constants.ActionType;
 import com.yulotte.subprime.policy.BrokeragePolicy;
 import com.yulotte.subprime.policy.BrokeragePolicyFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,13 @@ public class BrokerageQueryController {
     @GetMapping("/api/calc/brokerage")
     public Long calcBrokerage(@RequestParam ActionType actionType, @RequestParam Long price) {
         BrokeragePolicy policy = BrokeragePolicyFactory.getBrokeragePolicyBy(actionType);
+        return policy.calculate(price);
+    }
+
+    @GetMapping("/api/calc/apartment/{apartmentId}")
+    public Long calcBrokerageByApartmentId(@PathVariable Long apartmentId, @RequestParam ActionType actionType) {
+        BrokeragePolicy policy = BrokeragePolicyFactory.getBrokeragePolicyBy(actionType);
+        Long price = 0L; // TODO:  apartmentId로 금액가져오기
         return policy.calculate(price);
     }
 }
